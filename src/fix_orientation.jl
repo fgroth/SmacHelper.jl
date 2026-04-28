a12fbbfd4452963f137bd19b80ec2172c1361e4f
Frederick Groth <fgroth@usm.lmu.de>
2026-04-28 15:21:39 +0200

    add documentation

--- src/fix_orientation.jl
+++ src/fix_orientation.jl
@@ -1,4 +1,12 @@
 
+"""
+    permute_image_righthanded(data::Matrix{Float64}; north_axis::Int, east_axis::Int,
+                              north_east_angles::Vector{Float64}=[0.5*pi,0])
+
+Return the `data` Matrix rotated and mirrored such that the north axis points in y-direction, and the east axis in x-direction.
+
+The `north_east_angles` vector allows to obtain the adjusted angles.
+"""
 function permute_image_righthanded(data::Matrix{Float64}; north_axis::Int, east_axis::Int,
                                    north_east_angles::Vector{Float64}=[-0.5*pi,0])
     
@@ -33,6 +41,13 @@ function permute_image_righthanded(data::Matrix{Float64}; north_axis::Int, east_
     end
 end
 
+"""
+    keep_angle_in_limits(angle::Float64; lower_limit::Float64=-1*pi, upper_limit=+1*pi)
+
+Return adjusted angle within given limits.
+
+upper_limit - lower_limit should equal 2*pi.
+"""
 function keep_angle_in_limits(angle::Float64; lower_limit::Float64=-1*pi, upper_limit=+1*pi)
     while angle < lower_limit
         angle += 2*pi
