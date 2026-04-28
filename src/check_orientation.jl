2254102594efd0871f6a417ab2d4973b02d331e9
Frederick Groth <fgroth@usm.lmu.de>
2026-04-28 13:36:36 +0200

    add functions to determine closest axis to north/east direction

--- src/check_orientation.jl
+++ src/check_orientation.jl
@@ -38,3 +38,85 @@ function check_rotated_north_east_righthanded(fitsfile::String; los::Vector{Floa
 
     return check_north_east_righthanded(north_angle=north_angle, east_angle=east_angle)
 end
+
+"""
+    determine_best_image_orientation(fitsfile::String; los::Vector{Float64}, default_north::Vector{Float64}=[0,0,1.0])
+
+Return the north and east axis (x is 1, y is 2) closest to the north/east angles after the rotation done by smac/phox.
+"""
+function determine_best_image_orientation(fitsfile::String; los::Vector{Float64}, default_north::Vector{Float64}=[0,0,1.0])
+    east_angle = default_east_angle(fitsfile, los=los, default_north=default_north)
+    north_angle = default_north_angle(fitsfile, default_north=default_north)
+    # values are in the range [-pi, pi].
+    
+    #check which axis is closest to north:
+    north_axis = check_closest_aixs(north_angle)
+    # we should just check the general rotation between north and east. There could be rare edge-cases where east and north are just below pi/2 apart due to numerical rounding and they perfectly align with the limits between axis, which could lead to north and east being assigned to the same axis.
+    right_handed = check_north_east_angle_righthanded(north_angle=north_angle, east_angle_east_angle)
+    if right_handed
+        east_axis = right_axis(north_axis)
+    else
+        east_axis = left_axis(north_axis)
+    end
+    
+    return north_axis, north_axis
+end
+
+"""
+    check_closest_aixs(angle::Float64)
+
+Check which axis is closest to given rotation.
+"""
+function check_closest_aixs(angle::Float64)
+    if angle < -pi || angle > pi
+        error("angle has to be in the range [-pi,pi]")
+    end
+    if -0.25*pi < angle <= 0.25*pi
+        # x axis, positive direction
+        return 1
+    elseif 0.25*pi < angle <= 0.75*pi
+        # y axis, positive direction
+        return 2
+    elseif 0.75*pi < angle || angle <= -0.75*pi
+        # x axis, negative direction
+        return -1
+    elseif -0.75*pi < angle <= -0.25*pi
+        # y axis, negative direction
+        return -2
+    end
+end
+
+"""
+    right_axis(axis::Int)
+
+Return the axis right of the given one (negative rotation).
+"""
+function right_axis(axis::Int)
+    if axis == 1
+        return -2
+    elseif axis == -2
+        return -1
+    elseif axis == -1
+        return 2
+    elseif axis == 2
+        return 1
+    end
+    error("axis seems to be neither 1/-1, nor 2/-2. This is not allowed")
+end
+"""
+    left_axis(axis::Int)
+
+Return the axis left of the given one (positive rotation).
+"""
+function left_axis(axis::Int)
+    if axis == 1
+        return 2
+    elseif axis == 2
+        return -1
+    elseif axis == -1
+        return -2
+    elseif axis == -2
+        return 1
+    end
+    error("axis seems to be neither 1/-1, nor 2/-2. This is not allowed")
+end
