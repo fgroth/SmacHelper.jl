010b3d86afb708655132d404dcfda70ce761d670
Frederick Groth <fgroth@usm.lmu.de>
2026-05-12 17:14:26 +0200

    minor update to rvir plot: ellipse uses diameter, not radius, remove wrong factor 0.5; some cleanup of the function

--- src/plot_smac/plot_rvir.jl
+++ src/plot_smac/plot_rvir.jl
@@ -15,19 +15,19 @@ function plot_rvir_circle(ax::PyCall.PyObject; rvir_to_plot::Union{Real,Tuple{<:
     y_center = 0.5*sum(y_axis_limits)
     center = (x_center, y_center)
 
+    if rvir_to_plot == 0
+        return
+    end
+
     if isa(rvir_to_plot,Real) && x_length==y_length
-        # image is distorted, need to plot an ellipse of rvir
-        if rvir_to_plot != 0
-            rvir_scale = 0.5 / rvir_to_plot
-            ax.add_patch(matplotlib.patches.Circle(center,x_length.*rvir_scale,
-                                                   fill=false,color="gray",linestyle="dashed"))
-        end
-    else
         # circle of rvir
-        rvir_scale = 0.5 ./ rvir_to_plot
-        width_height = (x_length,y_length) .* rvir_scale
-        # switch indices due to column major order in julia.
-        ax.add_patch(matplotlib.patches.Ellipse(center,width_height[1],width_height[2],
+        ax.add_patch(matplotlib.patches.Circle(center,0.5 * x_length / rvir_to_plot,
+                                               fill=false,color="gray",linestyle="dashed"))
+    else
+        # image is distorted, need to plot an ellipse of rvir
+        width = x_length / rvir_to_plot
+        height= y_length / rvir_to_plot
+        ax.add_patch(matplotlib.patches.Ellipse(center,width,height,
                                                 fill=false,color="gray",linestyle="dashed"))
     end
 
